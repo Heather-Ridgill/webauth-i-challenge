bea7fdfd51592609ac29439d8fe18b2995c44b5c
Heather-Ridgill <Heather.Ridgill@gmail.com>
2019-12-04 19:45:19 -0800

    auth-router, migrations

---
+++ api/api-router.js
@@ -0,0 +1,12 @@
+const router = require (`express`).Router();
+
+const authRouter = require (`../auth/auth-router.js`);
+const usersRouter = require (`../users/users-router`);
+
+
+router.use (`./auth`, authRouter);
+
+router.use (`./users`, usersRouter);
+
+
+module.exports = router;
