af172bd3f985e49f591d9c58cb60f8f9faa070b5
Heather-Ridgill <Heather.Ridgill@gmail.com>
2019-12-06 20:07:04 -0800

    Finished MVP

--- api/server.js
+++ api/server.js
@@ -1,9 +1,12 @@
 const express = require (`express`);
 
+const configureMiddleware = require (`./configure-middleware`);
+
 const apiRouter = require (`./api-router`);
 
-
 const server = express();
+
+configureMiddleware(server);
 
 server.use (`/api`, apiRouter);
 
--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,40 +1,69 @@
-const router = require (`express`).Router();
+// const router = require(`express`).Router();
 
-const bcrypt = require(`bcryptjs`);
+// const bcrypt = require(`bcryptjs`);
 
-const Users = require (`../users/users-model`);
+// const Users = require(`../users/users-model`);
 
 
-router.post('/register', (req, res) => {
-    let user = req.body;
-    const hash = bcrypt.hashSync(user.password, 10);
-    user.password = hash;
+// router.post('/register', (req, res) => {
+//     let user = req.body;
+//     console.log(req.body)
+//     const hash = bcrypt.hashSync(user.password, 8);
+//     user.password = hash;
   
-    Users.add(user)
-      .then(saved => {
-        res.status(201).json(saved);
-      })
-      .catch(error => {
-        res.status(500).json(error);
-      });
-  });
+//     Users.add(user)
+//       .then(saved => {
+//         res.status(201).json(saved);
+//       })
+//       .catch(error => {
+//         res.status(500).json(error);
+//       });
+//   });
 
 
-router.post (`/login`, (req, res) => {
-    let { username, password } = req.body;
+// router.post (`/login`, (req, res) => {
+//     let { username, password } = req.body;
 
-    Users.findBy({ username })
-    .first()
-    .then(user => {
-        if (user && bcrypt.compareSync(password, user.password)) {
-            res.status(200).json({ message: `Welcome VIP Member ${user.username}!`});
-        } else {
-            res.status(401).json ({message: `Sorry, you have put invalid credentials`});
-        }
+//     Users.findBy({ username })
+//     .first()
+//     .then(user => {
+//         if (user && bcrypt.compareSync(password, user.password)) {
+//             res.status(200).json({ message: `Welcome VIP Member ${user.username}!`});
+//         } else {
+//             res.status(401).json ({message: `Sorry, you have put invalid credentials`});
+//         }
+//     })
+//     .catch(error => {
+//         res.status(500).json(error);
+//     });
+// });
+
+// module.exports = router;
+
+
+const router = require("express").Router();
+const bcrypt = require("bcryptjs");
+const authorize = require("./auth-required-middleware.js");
+
+const Users = require("../users/users-model.js");
+
+router.post("/register", (req, res) => {
+  let user = req.body;
+  const hash = bcrypt.hashSync(user.password, 8);
+  user.password = hash;
+
+  Users.add(user)
+    .then(saved => {
+      res.status(201).json(saved);
     })
     .catch(error => {
-        res.status(500).json(error);
+      res.status(500).json(error);
     });
+});
+
+router.post("/login", authorize, (req, res) => {
+  let { username } = req.headers;
+  res.status(200).json({ message: `Welcome ${username}!` });
 });
 
 module.exports = router;
